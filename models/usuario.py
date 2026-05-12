98d48c169ce2d7718317bc13a4eafaa2110add4f
ViniSouza001 <viniciussouza130705@gmail.com>
2026-05-11 23:56:06 -0300

    Refactor database connection and model imports by moving connection logic to a dedicated database.py file

--- models/usuario.py
+++ models/usuario.py
@@ -2,11 +2,7 @@ from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, F
 from enum import Enum as PyEnum
 from sqlalchemy.orm import declarative_base, relationship
 
-# cria a conexão do banco de dados
-db = create_engine("sqlite:///database/banco.db") # começa com 3 barras, se quiser criar uma pasta, é só digitar o nome da pasta/nome_do_arquivo
-
-# cria a base do banco
-Base = declarative_base()
+from database.database import Base
 
 class Usuario(Base):
     __tablename__ = "usuarios"
