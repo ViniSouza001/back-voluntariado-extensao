6dc1d400b797c4820718e3d578dd1c8eeb472188
ViniSouza001 <viniciussouza130705@gmail.com>
2026-06-08 10:59:11 -0300

    feat: add frontend pages and scripts for user registration, login, and data consultation

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:5500",
         "http://127.0.0.1:5173",
+        "http://127.0.0.1:52325",
     ],
     allow_credentials=True,
     allow_methods=["*"],
